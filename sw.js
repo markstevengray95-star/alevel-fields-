d1050782241739c325c8e1b51cd414566b943389
markstevengray95-star <markstevengray95@gmail.com>
2026-09-23 10:17:49 +0100

    Cache v3 textbook and simulation assets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "aqa-fields-lab-v2";
+const CACHE = "aqa-fields-lab-v3";
 const ASSETS = [
   "./",
   "./index.html",
@@ -6,32 +6,30 @@ const ASSETS = [
   "./further.css",
   "./fields.css",
   "./enhancements.css",
+  "./enhancements-v3.css",
   "./data.js",
   "./app.js",
   "./enhancements.js",
+  "./textbook-v3.js",
+  "./sim-pro-v3.js",
   "./physics-icon.svg",
   "./manifest.webmanifest"
 ];
-
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))));
   self.clients.claim();
 });
-
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    fetch(event.request).then(response => {
       const copy = response.clone();
       caches.open(CACHE).then(cache => cache.put(event.request, copy));
       return response;
-    }).catch(() => caches.match("./index.html")))
+    }).catch(() => caches.match(event.request).then(cached => cached || caches.match("./index.html")))
   );
 });
